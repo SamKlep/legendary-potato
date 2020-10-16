edfcc54610680794cd6cbfc20962b4930c894a9b
SamKlep <shamdemann23@gmail.com>
2020-10-16 11:09:16 -0500

    Heroku deploy.

---
+++ public/precache-manifest.3e5ccd02bad05434e6f62d6128fec581.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4812d0f8d4ec67dbe94ede1ea4ff027",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79e0e431a658f8277b54",
+    "url": "/static/css/main.540bc5b9.chunk.css"
+  },
+  {
+    "revision": "99771d6c08f1db0b5f1e",
+    "url": "/static/js/2.ea8a11c1.chunk.js"
+  },
+  {
+    "revision": "2c22650a9c32e101feb0bd356c0f9723",
+    "url": "/static/js/2.ea8a11c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79e0e431a658f8277b54",
+    "url": "/static/js/main.b39c34b2.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "402f6c88c5e372d914667d20c32991a3",
+    "url": "/static/media/spacex-logo.402f6c88.jpg"
+  }
+]);
